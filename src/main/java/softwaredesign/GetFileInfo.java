dcf2be17c5672ff60c5afeb90c135026a63dac8c
alexpaascual <76909137+alexpaascual@users.noreply.github.com>
2022-03-03 16:23:14 +0100

    sample

--- src/main/java/softwaredesign/GetFileInfo.java
+++ src/main/java/softwaredesign/GetFileInfo.java
@@ -9,7 +9,6 @@ public class GetFileInfo {
         File myObj = new File("sample.gpx");
         if (myObj.exists()) {
             System.out.println("File name: " + myObj.getName());
-            System.out.println("Absolute path: " + myObj.getAbsolutePath());
             System.out.println("Writeable: " + myObj.canWrite());
             System.out.println("Readable " + myObj.canRead());
             System.out.println("File size in bytes " + myObj.length());
