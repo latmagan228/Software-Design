555c2b2606f4c5090cb6788c0824439304fd82e2
alexpaascual <76909137+alexpaascual@users.noreply.github.com>
2022-03-09 16:29:41 +0100

    sample

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -4,14 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         FileStatus fileStatus = new FileStatus();
+        FileInfo fileInfo = new FileInfo();
+
         fileStatus.FileInfo();
-        GetFileInfo getFileInfo = new GetFileInfo();
-        getFileInfo.ReadFile();
+        fileInfo.ReadFile();
+
     }
 }
 
