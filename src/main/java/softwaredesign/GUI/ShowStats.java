d0beeab87bee9f50227f4ca3c3333d80805a0e3e
latmagan228 <latmagan228@gmail.com>
2022-03-24 16:08:49 +0100

    a

--- src/main/java/softwaredesign/GUI/ShowStats.java
+++ src/main/java/softwaredesign/GUI/ShowStats.java
@@ -6,7 +6,12 @@ import softwaredesign.Statistics;
 import javax.swing.*;
 import java.io.IOException;
 
-public class ShowStats extends JFrame{
+interface GPXSportPlugin {
+    String running = "running";
+    void Running() throws IOException;
+}
+
+public class ShowStats extends JFrame implements GPXSportPlugin {
 
     private JLabel dateLabel;
     private JLabel totalDisLabel;
@@ -15,9 +20,25 @@ public class ShowStats extends JFrame{
     private JLabel elevationDifLabel;
     private JPanel panelMain;
 
-
     public ShowStats() throws IOException {
         super("GPX Manager");
+
+        this.setContentPane(this.panelMain);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocation(600,150);
+        this.pack();
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] Args) throws IOException {
+        ShowStats sport = new ShowStats();
+        sport.Running();
+    }
+
+    @Override
+    public void Running() throws IOException {
 
         int timeDifference = Statistics.totalTime();
         int totalHours = timeDifference / 3600;
@@ -28,17 +49,6 @@ public class ShowStats extends JFrame{
         totalTimeLabel.setText("The total time completed of the track is: " + totalHours + " hours, " + totalMinutes + " minutes, " + timeDifference % 60 + " seconds." );
         averageSpeedLabel.setText("Average speed of the track is: " + Statistics.averageSpeed() + "km/h");
         elevationDifLabel.setText("The elevation difference in the track is: " + Statistics.altitudeDifference() + "m");
-
-        this.setContentPane(this.panelMain);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocation(600,150);
-        this.pack();
-        this.setVisible(true);
-
-    }
-
-    public static void main() throws IOException {
-        new ShowStats();
     }
 
 }
